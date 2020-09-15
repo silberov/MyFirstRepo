7f46d1597c42515f12f240c719ae79cc490454f5
Wilfredo <casas.farach@gmail.com>
2020-09-15 11:36:19 +0200

    student picker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const studentList = [
+    "Inna",
+    "Sebastian",
+    "Sophie T.",
+    "Fakhira",
+    "Annika",
+    "Said",
+    "Hamid",
+    "Omar",
+    "Guillerme",
+    "Sofia D.",
+    "Angel",
+  ];
+
+  function pickRandomStudent() {
+    if (studentList.length === 0) alert("You ran out of students!");
+    const randomIndex = Math.floor(Math.random() * studentList.length);
+    const randomStudent = studentList[randomIndex];
+    alert("A random student: " + randomStudent);
+    studentList.splice(randomIndex, 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => pickRandomStudent()}>Pick Someone!</button>
     </div>
   );
 }
